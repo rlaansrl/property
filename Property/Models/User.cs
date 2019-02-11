b74fb52e950b0e3f157206d2fc4020f2e3239be1
H701 <H701@365office.site>
2019-02-11 18:27:19 +0900

    김문기:유효성검사추가

--- Property/Models/User.cs
+++ Property/Models/User.cs
@@ -18,7 +18,7 @@ namespace Property.Models
         /// <summary>
         /// 회원 이름
         /// </summary>
-        [Required]
+        [Required(ErrorMessage ="이름은 필수입력 값입니다.")]
         public string UserName { get; set; }
 
         /// <summary>
@@ -31,7 +31,12 @@ namespace Property.Models
         /// 회원 비번
         /// </summary>
         [Required(ErrorMessage ="비밀번호는 필수입력 값입니다.")]
+        [DataType(DataType.Password)]
         public string UserPw { get; set; }
+
+        [DataType(DataType.Password)]
+        [Compare("Password",ErrorMessage ="비밀번호를 확인해 주세요.")]
+        public string ConfirmUserPw { get; set; }
 
         /// <summary>
         /// 회원 주소
@@ -43,14 +48,14 @@ namespace Property.Models
         /// 회원 전화번호
         /// </summary>
         [Required(ErrorMessage ="전화번호는 필수입력 값입니다.")]
-        [Phone]
+        [Phone(ErrorMessage ="전화번호 형식이 올바르지 않습니다.")]
         public string UserPhone { get; set; }
 
         /// <summary>
         /// 회원 이메일
         /// </summary>
         [Required(ErrorMessage ="이메일은 필수입력 값입니다.")]
-        [EmailAddress]
+        [EmailAddress(ErrorMessage ="이메일 형식이 올바르지 않습니다.")]
         public string UserEmail { get; set; }
 
         /// <summary>
